3899ca8832009dadfad5e1a62952cf46f0aede76
Vijaylad12 <ladv1310@gmail.com>
2024-08-18 07:48:45 -0400

    deconstructio of an object

--- learning/first.js
+++ learning/first.js
@@ -1,5 +1,13 @@
-let var1=25
-const var2=5
-var var3="my friend ganesha"
-var4="oh my god"
-console.table([var1,var2,var3,var4])
+// let var1=25
+// const var2=5
+// var var3="my friend ganesha"
+// var4="oh my god"
+// console.table([var1,var2,var3,var4])
+const student={
+    name:"vijaykumar gajanan lad",
+    age:19,
+    rollNo:30,
+    id:"2222300049"
+}
+let {name}=student
+console.log(name);
